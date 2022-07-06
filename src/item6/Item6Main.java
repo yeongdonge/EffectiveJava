4305071f91991043c965c0c7ad6ba96749366334
yeongdonge <parrineau@gmail.com>
2022-07-06 16:43:10 +0900

    item7

--- src/item6/Item6Main.java
+++ src/item6/Item6Main.java
@@ -4,7 +4,7 @@ import java.util.regex.Pattern;
 
 public class Item6Main {
 
-    private static final Pattern ROMAN = Pattern.compile("^(?=.)M*(C[MD]|D?C{0,3})" + "(X[CL]|L?X{0,3})(I[XV]|V?I{0,3})$")
+    private static final Pattern ROMAN = Pattern.compile("^(?=.)M*(C[MD]|D?C{0,3})" + "(X[CL]|L?X{0,3})(I[XV]|V?I{0,3})$");
 
     static boolean isRomanNumeral(String s) {
         return ROMAN.matcher(s).matches();
